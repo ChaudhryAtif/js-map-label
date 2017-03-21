72883b353a3d56081b5860c27378c7d14edb0614
Atif Chaudhry <achaudh5@slu.edu>
2017-03-21 18:44:16 -0500

    Update maplabel.js

--- src/maplabel.js
+++ src/maplabel.js
@@ -128,7 +128,7 @@ MapLabel.prototype.onAdd = function () {
 
     var panes = this.getPanes();
     if (panes) {
-        panes.floatPane.appendChild(canvas);
+        panes.overlayLayer.appendChild(canvas);
     }
 };
 MapLabel.prototype['onAdd'] = MapLabel.prototype.onAdd;
